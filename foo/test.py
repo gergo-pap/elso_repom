44a0381d2ab318391c214c01a700ec49ab455049
Gergo Pap <p.gergo99@gmail.com>
2019-02-02 16:39:29 +0100

    eddig jutottam

--- foo/test.py
+++ foo/test.py
@@ -2,5 +2,15 @@ def main():
     pass
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':sdf s
+f sd
+fs
+df
+sdf\'[ealfk\awt' \
+    'l\tmk\]\atm p\]\mjtp se' \
+    'g ,ktzs\'' \
+    'egm,k s\zmeg\zsmeg\
+,m zs
+g
+
     main()
