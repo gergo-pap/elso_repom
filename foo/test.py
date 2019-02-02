c9a626c3b9a77ac83e55ed97e5b2993a5e66cf6d
Gergo Pap <p.gergo99@gmail.com>
2019-02-02 17:05:41 +0100

    foo

--- foo/test.py
+++ foo/test.py
@@ -2,17 +2,5 @@ def main():
     pass
 
 
-if __name__ == '__main__':sdf s
-f sd
-fs
-df
-sdf\'[ealfk\awt' \
-    'l\tmk\]\atm p\]\mjtp se' \
-    'g ,ktzs\'' \
-    sdf;
-    s]dlf sdkmf sdkf\;aw m'fl naf' \
-                          ' l\a,w mf nla;'fnwk\afl \af
-,m zs
-g
-
+if __name__ == '__main__':
     main()
